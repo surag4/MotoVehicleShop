efb6f12d5855f3c41d747124c54d842e41171e99
surag4 <suragpandit@gmail.com>
2022-03-01 00:31:55 +1100

    homepage updated
    addition of flash sale in home page. where 3 items will be in sale every time

--- templates/users/userhomeTemplate.php
+++ templates/users/userhomeTemplate.php
@@ -6,7 +6,7 @@
         </div>
     
         <div class="searchbarProp">
-            <form action="index.php?page=searchProp" method="post">
+            <form action="index.php?page=filter" method="post">
                 <input type="text" name="query" required>
                 <input type="submit" name="findproperty" value="Search">
             </form>
@@ -18,8 +18,7 @@
     <h2>Trendy Used Scooter in Market</h2>
     <div class="slides">
         <?php  
-            foreach ($trendScooter as $value) {
-                $pidjs = $value['prop_id'];
+            for ($pidjs = 1; $pidjs <= 7; $pidjs++) {
         ?>
         <div class="vehicle">
             <div class="thumbnail_images"> 
@@ -57,8 +56,7 @@
     <h2>Trendy Used Bikes in Market</h2>
     <div class="slides">
         <?php  
-            foreach ($trendBike as $value) {
-                $pidjss = $value['prop_id'];
+            for ($pidjss = 3; $pidjss <= 7; $pidjss++) {
         ?>
         <div class="vehicle">
             <div class="thumbnail_images"> 
@@ -92,8 +90,7 @@
     <h2>Popular Bikes/Scooter</h2>
     <div class="slides">
         <?php  
-            foreach ($popular as $value) {
-                $pidjsss = $value['prop_id'];
+            for ($pidjsss = 2; $pidjsss <= 8; $pidjsss++) {
         ?>
         <div class="vehicle">
             <div class="thumbnail_images"> 
@@ -121,6 +118,38 @@
         </div>
         <?php } ?>
     </div>
+</section>
+
+<section class="flashsale">
+    <?php for ($j=0; $j < 3; $j++) { ?>
+        <div class="sale">
+        <div class="vehicle">
+                <div class="thumbnail_images"> 
+                    <img src="../images/1/1.jpg" alt="Image Link Broken" style="width:100%; height: 100%;" />
+                </div>
+    
+                <div class="details">
+                    <span class="nameicon"> <i class="fa-solid fa-motorcycle"></i> </span>
+                    <span class="name">Bajaj Pulsar 250</span>
+    
+                    <span class="distTravelledicon"> <i class="fas fa-tachometer-alt"></i> </span>
+                    <span class="distTravelled">6000 km</span>
+    
+                    <span class="locatedicon"> <i class="fas fa-map-marker-alt"></i> </span>
+                    <span class="located"> Kathmandu </span>
+                        
+                    <span class="ccicon"> <i class="fas fa-bolt"></i> </span>
+                    <span class="cc">125cc</span>
+    
+                    <span class="priceicon"> <i class="fas fa-rupee-sign"></i> </span>
+                    <span class="price">31000</span>
+    
+                    <a href="#" class="getvechicledetails"> Details </a>
+                </div>
+            </div>
+        </div>
+    <?php }?>
+    
 </section>
 
 </article>
