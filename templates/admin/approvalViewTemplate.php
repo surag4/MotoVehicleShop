3f604f1f636a12e1c58a1c25e1d6f3c6dbdec3ab
surag4 <suragpandit@gmail.com>
2022-03-06 00:47:25 +1100

    optimization in front end for approval page
    the layout of front end has been optimized

--- templates/admin/approvalViewTemplate.php
+++ templates/admin/approvalViewTemplate.php
@@ -1,37 +1,55 @@
-<section class="registration">
+<section class="veh-approval">
     <form action="index.php?page=approvalView" method="POST">
-        <div class="register-container">
+        <div class="approval-container">
             <h1>Approve A Vechicle</h1>
             <hr>
             <?php
             foreach($data as $value){
             ?>
-            <label for="name">Vechicle Name</label>
-            <input type="text" name="vec_name" placeholder="Vechicle Name" value="<?php echo $value['vec_name'];?>" disabled><br>
+            <div class="approvalsubcont">
+                <label for="name">Vechicle Name</label>
+                <input type="text" name="vec_name" placeholder="Vechicle Name" value="<?php echo $value['vec_name'];?>" disabled>
+            </div>
 
-            <label for="name">Vechicle Distance Covered</label>
-            <input type="number" name="vec_distance" placeholder="Vechicle Distance Covered" value="<?php echo $value['vec_distance'];?>" disabled><br><br>
+            <div class="approvalsubcont"> 
+                <label for="name">Vechicle Distance Covered</label>
+                <input type="number" name="vec_distance" placeholder="Vechicle Distance Covered" value="<?php echo $value['vec_distance'];?>" disabled>
+            </div>
 
-            <label for="user anme temp">Vechicle location</label>
-            <input type="text" placeholder="Vechicle location" name="vec_location" value="<?php echo $value['vec_location'];?>" disabled><br><br>
-            
-            <label for="name">Vechicle Engine CC</label>
-            <input type="number" placeholder="Vechicle Engine CC" name="vec_engine" value="<?php echo $value['vec_engine'];?>" disabled><br><br>
-        
-            <label for="name">Vechicle Price</label>
-            <input type="number" placeholder="Vechicle Price" name="vec_price" value="<?php echo $value['vec_price'];?>" disabled><br><br>
+            <div class="approvalsubcont"> 
+                <label for="user anme temp">Vechicle location</label>
+                <input type="text" placeholder="Vechicle location" name="vec_location" value="<?php echo $value['vec_location'];?>" disabled>
+            </div>
 
-             <label for="name">Vechicle Type</label>
-            <input type="text" placeholder="Vechicle Type" name="type" value="<?php echo $value['type'];?>" disabled><br><br>
+            <div class="approvalsubcont"> 
+                <label for="name">Vechicle Engine CC</label>
+                <input type="number" placeholder="Vechicle Engine CC" name="vec_engine" value="<?php echo $value['vec_engine'];?>" disabled>
+            </div>
 
-            <label for="name">Vechicle Brand</label>
-            <input type="text" placeholder="Vechicle brand" name="vec_brand" value="<?php echo $value['vec_brand'];?>" disabled><br><br>
+            <div class="approvalsubcont"> 
+                <label for="name">Vechicle Price</label>
+                <input type="number" placeholder="Vechicle Price" name="vec_price" value="<?php echo $value['vec_price'];?>" disabled>
+            </div>
 
-            <label for="name">Vechicle Model</label>
-            <input type="text" placeholder="Vechicle Model" name="vec_model" value="<?php echo $value['vec_model'];?>" disabled><br><br>
+            <div class="approvalsubcont"> 
+                <label for="name">Vechicle Type</label>
+                <input type="text" placeholder="Vechicle Type" name="type" value="<?php echo $value['type'];?>" disabled>
+            </div>
 
-            <label for="name">Vechicle Upload Date</label>
-            <input type="text" placeholder="Vechicle Upload Date" name="vec_upload_date" value="<?php echo $value['vec_upload_date'];?>" disabled><br><br>
+            <div class="approvalsubcont"> 
+                <label for="name">Vechicle Brand</label>
+                <input type="text" placeholder="Vechicle brand" name="vec_brand" value="<?php echo $value['vec_brand'];?>" disabled>
+            </div>
+
+            <div class="approvalsubcont"> 
+                <label for="name">Vechicle Model</label>
+                <input type="text" placeholder="Vechicle Model" name="vec_model" value="<?php echo $value['vec_model'];?>" disabled>
+            </div>
+
+            <div class="approvalsubcont"> 
+                <label for="name">Vechicle Upload Date</label>
+                <input type="text" placeholder="Vechicle Upload Date" name="vec_upload_date" value="<?php echo $value['vec_upload_date'];?>" disabled>
+            </div>
 
 
              <input type="hidden" name="custID" value="<?php echo $value['custID'];?>">
