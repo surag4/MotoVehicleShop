593fd47cffd8f5d4598d52acbb709c6125babf50
surag4 <suragpandit@gmail.com>
2022-03-14 20:07:09 +1100

    minor fix
    all the vehicles were giving the same image

--- templates/users/userhomeTemplate.php
+++ templates/users/userhomeTemplate.php
@@ -94,14 +94,15 @@
     <div class="slides">
         <?php  
             foreach ($popular as $value) {
-                $pidjsss = $value['vehicleID'];
+                $popvid = $value['vehicleID'];
         ?>
         <div class="vehicle">
             <div class="thumbnail_images"> 
-                <img src="../images/<?php echo $pidjss?>/1.jpg" alt="Image Link Broken" style="width:100%; height: 100%;" />
+                <img src="../images/<?php echo $popvid?>/1.jpg" alt="Image Link Broken" style="width:100%; height: 100%;" />
             </div>
 
             <div class="details">
+                <span><?php echo $popvid;?></span>
                 <span class="nameicon"> <i class="fa-solid fa-motorcycle"></i> </span>
                 <span class="name"><?php echo $value['modelName'];?></span>
 
