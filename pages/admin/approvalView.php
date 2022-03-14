9a1e8e30b8808d52574cab7136d1bd9f505aa0e1
surag4 <suragpandit@gmail.com>
2022-03-14 17:25:01 +1100

    changes

--- pages/admin/approvalView.php
+++ pages/admin/approvalView.php
@@ -2,16 +2,18 @@
 	$title = "Safari Express";
 	$data=[];
 	$conn = new DatabaseTable('vehicle');
+	$vehicle_id=0;
 	if(isset($_GET['vid'])){
-		$data=$conn->find("vec_id",$_GET['vid']);
+        $vehicle_id=$_GET['vid'];
+		$data=$conn->findVec($vehicle_id);
 	}
 
 	if(isset($_POST['approve'])){
-		$conn2 = new DatabaseTable('vehicle_category');
+		$conn2 = new DatabaseTable('vehiclecategory');
 		//$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
 
 		$val = array(
-			"vec_id"=>$_POST['vec_id'],
+			"vehicleID"=>$vehicle_id,
 			"hitcount"=>"0",
 			"category"=>"normal"
 		);
@@ -19,16 +21,16 @@
 		$conn2->insert($val);
 
 		$val2 = array(
-			"upload_status"=>1,
-			"vec_id"=>$_POST['vec_id']
+			"uploadStatus"=>1,
+			"vehicleID"=>$vehicle_id
 		);
 
-		$conn->update($val2,"vec_id");
-
+		$conn->update($val2,"vehicleID");
+		header('location:index.php?page=approve');
 	}
 
 	if(isset($_POST['discard'])){
-		$conn->delete("vec_id", $_POST['vec_id']);
+		$conn->delete("vehicleID", $_POST['vehicleID']);
 	}
 
 	if(isset($_POST['cancel'])){
