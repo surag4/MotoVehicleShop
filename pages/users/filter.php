14d5d5a274933ef0b8141d18788e6e9079199046
surag4 <suragpandit@gmail.com>
2022-03-14 21:52:47 +1100

    viewmore button will guide to filter page displaying all the results
    Search results are still capped at 1000. Might have to update if the website will be in need of more results

--- pages/users/filter.php
+++ pages/users/filter.php
@@ -1,8 +1,30 @@
 <?php
     $title="Search Results";
-    $conn_brand = new DatabaseTable('manufacturer');
-	$brands=$conn_brand->brandmodel();
-    $brandlist=$conn_brand->findAll('manufacturer');
 
-	$content = loadTemplate('../templates/users/searchTemplate.php', ['brands'=>$brands,'brandlist'=>$brandlist]);//load template
+    if(isset($_POST['trendScooter'])){
+        $conn = new DatabaseTable('vehicle');
+        $limit=10000;
+        $viewResults=$conn->orderOnVehicleType('Scooter',$limit);
+        $content = loadTemplate('../templates/users/searchTemplate.php', ['viewResults'=>$viewResults]);//load template
+    }
+
+    if(isset($_POST['trendBike'])){
+        $conn = new DatabaseTable('vehicle');
+        $limit=10000;
+        $viewResults=$conn->orderOnVehicleType('Bike',$limit);
+        $content = loadTemplate('../templates/users/searchTemplate.php', ['viewResults'=>$viewResults]);//load template
+    }
+
+    if(isset($_POST['popular'])){
+        $conn = new DatabaseTable('vehicle');
+        $limit=10000;
+        $viewResults=$conn->orderOn('price',$limit);
+        $content = loadTemplate('../templates/users/searchTemplate.php', ['viewResults'=>$viewResults]);//load template
+    }
+
+    // $conn_brand = new DatabaseTable('manufacturer');
+	// $brands=$conn_brand->brandmodel();
+    // $brandlist=$conn_brand->findAll('manufacturer');
+
+	// $content = loadTemplate('../templates/users/searchTemplate.php', ['brands'=>$brands,'brandlist'=>$brandlist]);//load template
 ?>
