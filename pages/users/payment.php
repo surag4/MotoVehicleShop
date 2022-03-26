bac42516b15651b48cf11b8e591396c896c19460
Image-Bhandari <50731001+Image-Bhandari@users.noreply.github.com>
2022-03-26 22:10:29 +0545

    changes for the day

--- pages/users/payment.php
+++ pages/users/payment.php
@@ -7,16 +7,56 @@
             $connToInsert-> insert($_POST);
             if($connToInsert){
                 header('location:index.php?page=');
-                //2147483647
-//
+          
 }
         }
         if(isset($_POST['esewa'])){
         $conn1 = new DatabaseTable('payment_esewa');
+        $shid=$_POST['shid'];
         unset($_POST['esewa']);
         $insertVal= $conn1->insert($_POST);
+
+        $conn1SH = new DatabaseTable('shortlist');
+        $val = [
+            "shortlistid"=>$shid,
+            "status"=>"pending"
+        ];
+
+        $updateToSH = $conn1SH->update($val,"shortlistid");
         }
 
+        if(isset($_POST['card_submit'])){
+        $conn2 = new DatabaseTable('creditinfo');
+        $shid=$_POST['shid'];
+        unset($_POST['card_submit']);
+        $insertVal= $conn2->insert($_POST);
+
+        $conn1SH = new DatabaseTable('shortlist');
+        $val = [
+            "shortlistid"=>$shid,
+            "status"=>"pending"
+        ];
+
+        $updateToSH = $conn1SH->update($val,"shortlistid");
+
+        }
+
+         if(isset($_POST['cash_on_deliv'])){
+        $conn3 = new DatabaseTable('cashondelivery');
+        $shid=$_POST['shid'];
+        unset($_POST['cash_on_deliv']);
+        $insertVal= $conn3->insert($_POST);
+
+        $conn1SH = new DatabaseTable('shortlist');
+        $val = [
+            "shortlistid"=>$shid,
+            "status"=>"pending"
+        ];
+
+        $updateToSH = $conn1SH->update($val,"shortlistid");
+        }
+
+        ///cash_on_deliv
         $content = loadTemplate('../templates/users/paymentTemplate.php', []);//load template
     }else {
         session_unset();
