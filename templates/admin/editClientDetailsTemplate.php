b64f08ab2c21497579499bb1795924b120a7e1c0
surag4 <suragpandit@gmail.com>
2022-03-29 14:44:10 +1100

    clients
    client details can be viewed by staffs/admins

--- templates/admin/editClientDetailsTemplate.php
+++ templates/admin/editClientDetailsTemplate.php
@@ -3,12 +3,12 @@
   <h2><?php if($data){echo "Update Client Details";}?></h2>
   <p><?php echo $status; ?></p>
   <form action="index.php?page=editClientDetails" method="post">
-    <input type="hidden" name="user_id" id="user_id" value=<?php if($data){echo $data['user_id'];}?>>
+    <input type="hidden" name="user_id" id="userID" value=<?php if($data){echo $data['userID'];}?>>
     <label for="firstname">First Name</label>
-    <input type="text" id="firstname" name="firstname" value="<?php if($data){echo $data['firstname'];}?>" required placeholder="Client's updated first name..">
+    <input type="text" id="firstname" name="firstName" value="<?php if($data){echo $data['firstName'];}?>" required placeholder="Client's updated first name..">
 
     <label for="lname">Last Name</label>
-    <input type="text" id="lastname" name="lastname" value="<?php if($data){echo $data['lastname'];}?>" required placeholder="Client's updated last name..">
+    <input type="text" id="lastname" name="lastName" value="<?php if($data){echo $data['lastName'];}?>" required placeholder="Client's updated last name..">
 
     <label for="email">Email</label>
     <input type="email" id="email" name="email" value="<?php if($data){echo $data['email'];}?>" required placeholder="Updated Email Address">
--- templates/admin/findclientsTemplate.php
+++ templates/admin/findclientsTemplate.php
@@ -2,22 +2,14 @@
 
     <div class="add">
         <p> LIST OF CLIENTS </p>
-        <div class="searchbar">
-            <form action="index.php?page=search" method="post">
-                <input type="text" name="query" required>
-                <input type="submit" name="findclients" value="Search">
-            </form>
-        </div>
     </div>
     <table id="listClients">
 
-    <tr><th>First Name</th><th>Last Name</th><th>E-mail Address</th><th>Edit Details</th><th>History</th> </tr>
+    <tr><th>First Name</th><th>Last Name</th><th>E-mail Address</th><th>Edit Details</th> </tr>
 
     <?php
     foreach ($data as $value) {
-        echo '<tr><td>'.$value['firstname'].'</td><td>'.$value['lastname'].'</td><td>'.$value['email'].'</td><td><a href="index.php?page=editClientDetails&cid='.$value['user_id'].'">Edit</a></td><td>
-        <a href="index.php?page=clienthistory&uid='.$value['user_id'].'">Check</a>
-        </td></tr>';
+        echo '<tr><td>'.$value['firstName'].'</td><td>'.$value['lastName'].'</td><td>'.$value['email'].'</td><td><a href="index.php?page=editClientDetails&cid='.$value['userID'].'">Edit</a></td></tr>';
     }
     ?>
 
