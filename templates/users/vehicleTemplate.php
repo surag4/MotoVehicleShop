4f5b666d051dfd89beca4ed215f68f82d3cb4e31
Image-Bhandari <50731001+Image-Bhandari@users.noreply.github.com>
2022-04-01 18:01:23 +0545

    changes

--- templates/users/vehicleTemplate.php
+++ templates/users/vehicleTemplate.php
@@ -41,22 +41,12 @@ foreach($data as $value){
         </div>
         
         <?php if($shortlisted && $_SESSION['login']!=0){
-<<<<<<< Updated upstream
             echo '<div class="buybtn"> <a href=index.php?page=viewvehicle&dshid='.$shortlisted['shortlistid'].'&vid='.$pid.'&ermsg=0>';
             if($buy==1){
                 echo '<a href=index.php?page=payment&shid='.$shortlisted['shortlistid'].'><button class="buyButton">BUY</button></a></div>';
             } 
         } elseif(!$shortlisted && $_SESSION['login']!=0) {
             echo '<a href=index.php?page=viewvehicle&shvid='.$pid.'&uid='.$_SESSION['userID'].'>';
-=======
-        echo '<a href=index.php?page=viewvehicle&dshid='.$shortlisted['shortlistid'].'&vid='.$pid.'&ermsg=0>';
-        if($buy==1 && $shortlisted['custID']!=$_SESSION['userID']){
-        echo '<a href=index.php?page=payment&shid='.$shortlisted['shortlistid'].'><button class="button">BUY</button></a>';
-        } 
-       }
-        elseif(!$shortlisted && $_SESSION['login']!=0) {
-        echo '<a href=index.php?page=viewvehicle&shvid='.$pid.'&uid='.$_SESSION['userID'].'>';
->>>>>>> Stashed changes
         }
         else{
             echo '<a href=index.php?page=login>';
